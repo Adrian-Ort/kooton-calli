127ac81ef828b5ab0cc61201c813be3521637432
Jose Angel Tirado <j.angel.tirado20@gmail.com>
2025-09-12 17:36:47 -0600

    add: import added in main.js

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Importar el CSS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importar el JS de Bootstrap (con Popper incluido)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Si tienes tu propio CSS, ponlo después para que sobrescriba
+import './CSS/style.css';
