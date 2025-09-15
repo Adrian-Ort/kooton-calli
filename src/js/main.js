7df6b37ca8a2e6162c19472f7f004e736b808542
fragosonic <fragosonic98@gmail.com>
2025-09-14 19:31:37 -0600

    Fix: there was an importation mistake in main.js. Translated coments

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // CSS
-import '../CSS/normalize.css';         // reset primero
-import '../CSS/global_variables.css';  // variables globales
-// import '../CSS/main.css;            // estilos principales      
+import '../CSS/normalize.css';         // first reset to normalize
+import '../CSS/global_variables.css';  // global variables
+import '../CSS/main.css';  // main styles
 
 // Componentes
 import './components.js';
