65468c493b038f7197ac819f61e149a1f887efbb
Yeudiel Avelar <avelar.martinezyeudiel@gmail.com>
2025-10-06 18:22:38 -0600

    fix: sign up

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,10 @@
-function getLogin() {
+export function getLogin() {
       const data = localStorage.getItem
       ('register');
       return data ? JSON.parse(data) : [];
 }
 
-function saveInputs(register) {
+export function saveInputs(register) {
       localStorage.setItem('register' , JSON.stringify(register));
 }
 
@@ -19,10 +19,11 @@ document.getElementById('form-login').addEventListener ('submit' , (e) =>{
       if (!validateEmail(email)) return alert ("Email inválido");
       if (!validatePassword(password)) return alert ("Contraseña inválida");
 
-      const newLogin = {
-            correo: email,
-            contraseña: password
-      };
+
+      //const newLogin = {
+           // correo: email,
+          //  contraseña: password
+      //};
 
       //Save in localStorage
       const login = getLogin();
@@ -31,7 +32,19 @@ document.getElementById('form-login').addEventListener ('submit' , (e) =>{
 
       alert("Sesión guardada en localStorage");
 
+      // Validation functions
+      const userValidated = login.find(login =>
+            login.email === email && login.password === password
+      );
+      
+      if (userValidated){
+            alert ("Ingreso correcto, Bienvenido.");
+      } else {
+            alert("Email o contraseña incorrectos");
+      }
+
       e.target.reset();
 }); 
 
-// Validation functions
+
+
